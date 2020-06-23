149c6f952c9032ad4de600d25dab5923f7b6ca4c
Igor-Polonskiy <53859088+Igor-Polonskiy@users.noreply.github.com>
2020-06-23 12:46:09 +0300

    hot fix

--- lection30/script.js
+++ lection30/script.js
@@ -4,10 +4,17 @@
  push.addEventListener('click', (e) => {
      const req = window.frames[0].document.querySelector('.request')
      let number = prompt("Введите число", );
-     if (number.match(/\D/i) == null) {
+     if (number == '') {
+         alert('введите любое число');
+     } else if (number.match(/\D/)) {
+         alert(' введите ЧИСЛО! ЦИФРАМИ!!');
+     } else if (number.match(/\d/g)[0] == 0 && number.match(/\d/g).length > 1) {
+         alert('число не может начинаться с 0');
+     } else {
          req.innerText = number;
          answer.innerText = (+req.innerText + 1);
-     } else alert('введите ЧИСЛО! ЦИФРАМИ!!');
+     };
+
  })
 
  window.onbeforeunload = function () {
